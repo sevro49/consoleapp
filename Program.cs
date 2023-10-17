d5050eb78a5141ca11cd4a1c0d5429bfd10df39d
sevro49 <21120205303@ismu.edu.tr>
2023-10-17 17:26:31 +0300

    variables

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int sayi = 10;
+double sayi2 = 10.2;
+decimal sayi3 = 11.2m;
+
+string name = "Emre Güler";
+char ch = 'Z';
+
+bool isRetired = true;
+
+
+Console.WriteLine(sayi);
+System.Console.WriteLine(sayi2);
+System.Console.WriteLine(sayi3);
