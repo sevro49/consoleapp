8d40a2b6c112ca624c81df3fb5f66e7e5fceb89f
sevro49 <21120205303@ismu.edu.tr>
2023-10-19 16:28:59 +0300

    Method parameters

--- Program.cs
+++ Program.cs
@@ -67,20 +67,39 @@ namespace consoleapp
         }
     }
 
+    class Calculcation
+    {
+        // public int Addition(int x, int y) { 
+        //     return x + y;
+        // }
+
+        public int Addition(params int[] numbers) { 
+
+            int sum = 0;
+            foreach(var num in numbers){
+                sum += num;
+            }
+            return sum;
+        }
+    }
 
     public class Program
     {
         static void Main(string[] args)
         {
-            var opel = new Car() { Brand = "Opel", Model = "Astra", Color = "White", IsAutoTransmission = true };
-            // opel.Start();
-            // opel.Stop();
-            // opel.SlowDown();
-            // opel.Accelerate();
-            // opel.Menu();
+            // var opel = new Car() { Brand = "Opel", Model = "Astra", Color = "White", IsAutoTransmission = true };
+            // // opel.Start();
+            // // opel.Stop();
+            // // opel.SlowDown();
+            // // opel.Accelerate();
+            // // opel.Menu();
 
-            var mazda = new Car() { Brand = "Mazda", Model = "Rx-7", Color = "Red", IsAutoTransmission = true };
-            mazda.Menu();
+            // var mazda = new Car() { Brand = "Mazda", Model = "Rx-7", Color = "Red", IsAutoTransmission = true };
+            // mazda.Menu();
+
+            var calculate = new Calculcation();
+            System.Console.WriteLine(calculate.Addition(5,2,6,8,43,3));  
+
         }
 
 
